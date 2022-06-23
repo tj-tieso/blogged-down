c43d853e142f22ee01cc11e0ad5ef83ece9be06e
tj-tieso <tj.tieso@gmail.com>
2022-06-23 15:48:12 -0500

    user can add comment when logged in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,6 +35,46 @@ router.get("/", (req, res) => {
     });
 });
 
+// RENDER SINGLE POST
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No Post found with that id." });
+        return;
+      }
+
+      const post = dbPostData.get({ plain: true });
+
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 // render login
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
