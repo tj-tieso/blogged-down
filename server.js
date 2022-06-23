5201ba3a4878b663d637e6798b6fdaffb5139931
tj-tieso <tj.tieso@gmail.com>
2022-06-23 11:20:43 -0500

    views folder setup

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-// const path = require("path");
+const path = require("path");
 const express = require("express");
 const routes = require("./controllers");
-// const exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 const sequelize = require("./config/connection");
 const session = require("express-session");
@@ -23,12 +23,13 @@ const PORT = process.env.PORT || 3001;
 // // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public"))); // take all of the contents "public" folder and serve them as static assets
+app.use(express.static(path.join(__dirname, "public"))); // take all of the contents "public" folder and serve them as static assets
 // // hb middleware
 // app.engine("handlebars", hbs.engine);
-// app.set("view engine", "handlebars");
+app.set("view engine", "handlebars");
 
 app.use(session(sess));
+const hbs = exphbs.create({}); // helpers go here
 
 app.use(routes);
 
