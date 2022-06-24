6e809a62046cafec087f2325ce076c0fbed4dd75
tj-tieso <tj.tieso@gmail.com>
2022-06-24 15:26:22 -0500

    authentication added

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const sequelize = require("../../config/connection");
+const withAuth = require("../../utils/auth");
 const { Post, User, Comment } = require("../../models");
 
 router.get("/", (req, res) => {
@@ -64,7 +65,7 @@ router.get("/:id", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
+router.post("/", withAuth, (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_content: 'string', user_id: 1}
   Post.create({
     title: req.body.title,
@@ -78,7 +79,7 @@ router.post("/", (req, res) => {
     });
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", withAuth, (req, res) => {
   Post.update(
     {
       title: req.body.title,
@@ -103,7 +104,7 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", withAuth, (req, res) => {
   console.log("id", req.params.id);
   Post.destroy({
     where: {
